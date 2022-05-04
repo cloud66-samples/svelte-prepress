ff7a789abe27072e647998649f06dffebd7f34d8
Khash Sajadi <khash@sajadi.co.uk>
2022-05-04 10:29:32 -0700

    init
    Signed-off-by: Khash Sajadi <khash@sajadi.co.uk>

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<main>
+<article>
+<div class="mx-auto">
+    <div class="relative overflow-hidden">
+    <div class="absolute inset-0">
+        <img class="h-full w-full object-cover" src="/splash.png" alt="Salute" />
+        <div class="absolute inset-0 bg-yellow-700 mix-blend-multiply"></div>
+    </div>
+    <div class="relative h-screen">
+        <div>
+        <a href="https://cloud66.com">
+            <img class="absolute inset-0 h-8 w-8 mt-4 ml-4" src="/logo.svg" alt="Cloud 66 Prepress" />
+        </a>
+        </div>
+        <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl pt-24">
+        <span class="block text-white">Hello,</span>
+        <span class="block text-yellow-200">it's good to see you here!</span>
+        </h1>
+        <p class="mt-6 max-w-lg mx-auto text-center text-xl text-orange-200 sm:max-w-3xl">
+        We're excited to see you trying out Cloud 66 Prepress, the best way to deploy your static sites on any cloud.
+        </p>
+        <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+        <div class="space-y-4 sm:space-y-0 sm:mx-auto">
+            <a href="https://www.cloud66.com/frameworks/static-sites" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-orange-700 bg-white hover:bg-orange-50 sm:px-8"> Find out more </a>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</article>
+</main>
