d0454ebd84c2c740e0c8145db28df5d5274bc289
Khash Sajadi <khash@sajadi.co.uk>
2022-05-04 12:56:01 -0700

    use static adapter
    Signed-off-by: Khash Sajadi <khash@sajadi.co.uk>

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { time_ranges_to_array } from 'svelte/internal';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			default: true,
+		}
 	}
 };
 
